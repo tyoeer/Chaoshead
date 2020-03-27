8c92c5cc0bc43e393dad26e348560e8c67548b55
tyoeer <tyoeer@users.noreply.github.com>
2020-03-27 15:20:24 +0100

    added a csv parser

---
+++ utils/csv.lua
@@ -0,0 +1,43 @@
+local csv = {}
+
+function csv.parseString(text,seperator)
+	parser = parser or csv.noParser
+	local SEP = seperator or ","
+	--print(parser("hey(hey)",false))
+	local END = "\n"
+	
+	local out
+	local headers = {}
+	local column = 1
+	local current = ""
+	
+	for i=1,text:len(),1 do
+		local char = text:sub(i,i)
+		if char==SEP then
+			if out and current then
+				out[#out][headers[column]] = current
+			else
+				headers[column] = current
+			end
+			current = ""
+			column = column + 1
+		elseif char==END then
+			if out then
+				out[#out][headers[column]] = current
+			else
+				headers[column] = current
+				out = {}
+			end
+			out[#out + 1] = {}
+			column = 1
+			current = ""
+		else
+			--print(i,char)
+			current = current .. char
+		end
+	end
+	
+	return out, headers
+end
+
+return csv
