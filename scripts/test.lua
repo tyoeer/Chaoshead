a999f3db7c5224e9d241faa1f8b53cc4ba1e2e8b
tyoeer <tyoeer@users.noreply.github.com>
2020-04-20 19:07:25 +0200

    Updated the test script

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,8 +1,19 @@
 local O = require("levelhead.objects.base")
+local P = require("levelhead.objects.propertiesBase")
 
-for i=10,15,1 do
+--cross
+for i=11,15,1 do
 	level:addObject(O:new(1),i,i)
-	if i~=3 then
-		level:addObject(O:new(1),6-i,i)
+	if i~=13 then
+		level:addObject(O:new(1),26-i,i)
 	end
+end
+
+--relays
+for i=1,level.width,1 do
+	local o = P:new(157)--relay
+	o:setSendingChannel(i)
+	o:setReceivingChannel(i-1)
+	--o:setSwitchRequirements(??)
+	level:addObject(o, i,1)
 end
