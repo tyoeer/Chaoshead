727828f821b8b0a4074d28427da341415d9c4584
tyoeer <tyoeer@users.noreply.github.com>
2020-04-29 17:15:37 +0200

    Made the test script use the new level editing features

--- scripts/test.lua
+++ scripts/test.lua
@@ -3,15 +3,13 @@ local P = require("levelhead.objects.propertiesBase")
 
 --cross
 for i=11,15,1 do
-	level:addObject(O:new(1),i,i)
-	if i~=13 then
-		level:addObject(O:new(1),26-i,i)
-	end
+	level:placeEnvironment(i,i)
+	level:placeEnvironment(26-i,i)
 end
 
 --relays
 for i=1,level.width,1 do
-	local o = P:new(157)--relay
+	local o = P:new("Relay")
 	o:setSendingChannel(i)
 	o:setReceivingChannel(i-1)
 	--o:setSwitchRequirements(??)
