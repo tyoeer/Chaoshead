afbc299abc0e4b2adf9c8ef4b347577567f2c063
tyoeer <tyoeer@users.noreply.github.com>
2020-04-29 17:12:23 +0200

    Fixed the level elements data

--- levelhead/data/elements.lua
+++ levelhead/data/elements.lua
@@ -4,13 +4,13 @@ local E = Class(require("levelhead.data.base"))
 function E:initialize()
 	--parse data file
 	local rawHeaders
-	self.data, rawHeaders = csv.parseString(love.filesystem.read("data/properties.tsv"),"\t")
+	self.data, rawHeaders = csv.parseString(love.filesystem.read("data/levelElements.tsv"),"\t")
 
 	--parse headers
 	self.headers = {}
 	for i,v in ipairs(rawHeaders) do
 		local raw = v:gsub("%W",""):lower()
-		if raw:match("name") then
+		if raw:match("^name$") then
 			self.headers.name = v
 		elseif raw:match("iddecimal") then
 			self.headers.id = v
@@ -18,4 +18,4 @@ function E:initialize()
 	end
 end
 
-return P
+return E
