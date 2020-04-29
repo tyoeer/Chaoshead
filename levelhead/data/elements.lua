14bf6941a29f2e884d8853383cd75b94fbaa4ec9
tyoeer <tyoeer@users.noreply.github.com>
2020-04-29 16:58:06 +0200

    Added level element data

---
+++ levelhead/data/elements.lua
@@ -0,0 +1,21 @@
+local csv = require("utils.csv")
+local E = Class(require("levelhead.data.base"))
+
+function E:initialize()
+	--parse data file
+	local rawHeaders
+	self.data, rawHeaders = csv.parseString(love.filesystem.read("data/properties.tsv"),"\t")
+
+	--parse headers
+	self.headers = {}
+	for i,v in ipairs(rawHeaders) do
+		local raw = v:gsub("%W",""):lower()
+		if raw:match("name") then
+			self.headers.name = v
+		elseif raw:match("iddecimal") then
+			self.headers.id = v
+		end
+	end
+end
+
+return P
