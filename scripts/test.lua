3a355a01b345a28f2d7e9eee11a54e5f0c172c05
tyoeer <tyoeer@users.noreply.github.com>
2020-04-30 16:00:38 +0200

    Added conveyors that go to the 0 in the test script.

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,6 +1,6 @@
 local O = require("levelhead.objects.base")
 local P = require("levelhead.objects.propertiesBase")
-
+--[[
 --cross
 for i=11,15,1 do
 	level:placeEnvironment(i,i)
@@ -14,4 +14,11 @@ for i=1,level.width,1 do
 	o:setReceivingChannel(i-1)
 	--o:setSwitchRequirements(??)
 	level:addObject(o, i,1)
+end
+]]--
+--conveyors
+for i=2,level.width-1,1 do
+	local o = level:placeToeSlider(i,27)
+	o:setDirection(0)
+	o:setSpeed(700)
 end
