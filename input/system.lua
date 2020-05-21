d20561e4c17f8cf671d1b57190db06fccff4bc43
tyoeer <tyoeer@users.noreply.github.com>
2020-05-21 16:37:33 +0200

    Refactored the input system to be much more modular

--- input/system.lua
+++ input/system.lua
@@ -1,113 +1,46 @@
 local input = {
-	triggers = {}
+	modules = {},
 }
 
--- KEYBOARD
+-- CALLBACKS (all for public use)
 
-input.triggers.keyboard = {}
+function input.actionActivated(name,isCursorBound,group) end
+function input.actionDeactivated(name,isCursorBound,group) end
 
-function input.keypressed(key, scancode, isrepeat)
-	if input.triggers.keyboard[key] then
-		for _,v in ipairs(input.triggers.keyboard[key]) do
-			input.activate(v)
-		end
-	end
-end
-
-function input.keyreleased(key, scancode)
-	if input.triggers.keyboard[key] then
-		for _,v in ipairs(input.triggers.keyboard[key]) do
-			input.deactivate(v)
-		end
-	end
-end
-
-function input.isKey(keycode)
-	-- this works because love.keyboard.isDown() errors when using an invalid keycode
-	-- as of Löve2d 11.3, there's no better way
-	local success = pcall(function() love.keyboard.isDown(keycode) end)
-	return success
-end
-
--- MOUSE
-
-input.triggers.mouse = {}
-
-function input.mousepressed(x, y, button, istouch, presses)
-	if input.triggers.mouse[button] then
-		for _,v in ipairs(input.triggers.mouse[button]) do
-			input.activate(v)
-		end
-	end
-end
-
-function input.mousereleased(x, y, button, istouch, presses)
-	if input.triggers.mouse[button] then
-		for _,v in ipairs(input.triggers.mouse[button]) do
-			input.deactivate(v)
-		end
-	end
-end
-
-local mouseAliases = {
-	left = 1,
-	right = 2,
-	middle = 3,
-}
-
-function input.verifyMouseButton(b)
-	--check for an alias
-	if mouseAliases[b] then
-		b = mouseAliases[b]
-	end
-	if pcall(function() love.mouse.isDown(b) end) then
-		return b
-	else
-		return false
-	end
-end
-
--- MAIN
-
-function input.addTrigger(type,button, data)
-	if not input.triggers[type][button] then
-		input.triggers[type][button] = {}
-	end
-	table.insert(input.triggers[type][button], data)
-end
+-- ACTIONS
 
 function input.parseButton(selector)
 	selector = selector:lower()
-	local type, button
+	local moduleName, button
 	if selector:find("%:") then
-		type, button = string.match(selector,"^(%w+)%:%s?(%w-)$")
-		if type=="key" or type=="keyboard" then
-			type = "keyboard"
-			if not input.isKey(button) then
-				error(button.." is not a valid key!")
-			end
-		elseif type=="mouse" then
-			button = input.verifyMouseButton(button)
-			if not button then
-				error(button.." is not a valid key!")
-			end
+		moduleName, button = string.match(selector,"^(%w+)%:%s?(%w-)$")
+		local module = input.modules[moduleName]
+		b = module:verify(button)
+		if b then
+			return moduleName, b
+		else
+			error("Invalid button "..button.." for module "..moduleName.."!")
 		end
 	else
-		button = selector
-		if input.isKey(button) then
-			type = "keyboard"
-		else
-			local mButton = input.verifyMouseButton(button)
-			if mButton then
-				type = "mouse"
-			else
-				error("Couldn't find valid input device for: "..button)
+		for moduleName,module in pairs(input.modules) do
+			button = module:verify(selector)
+			if button then
+				return moduleName,button
 			end
 		end
+		error("Invalid button: "..selector)
 	end
-	return type, button
 end
 
+function input.addTrigger(moduleName, button, action)
+	local t = input.modules[moduleName].triggers
+	if not t[button] then
+		t[button] = {}
+	end
+	table.insert(t[button], action)
+end
+
+--for public use
 function input.addAction(action,name,group)
 	local parsed = {
 		name = name,
@@ -115,12 +48,12 @@ function input.addAction(action,name,group)
 		active = false,
 	}
 	if action.trigger then
-		local type, button = input.parseButton(action.trigger)
+		local moduleName, button = input.parseButton(action.trigger)
 		parsed.trigger = {
-			type = type,
+			moduleName = moduleName,
 			button = button,
 		}
-		input.addTrigger(type,button, parsed)
+		input.addTrigger(moduleName,button, parsed)
 	end
 	if action.isCursorBound==nil then
 		parsed.isCursorBound = false
@@ -129,6 +62,7 @@ function input.addAction(action,name,group)
 	end
 end
 
+--for public use
 function input.parseActions(actions)
 	for group, entries in pairs(actions) do
 		for name, action in pairs(entries) do
@@ -137,22 +71,108 @@ function input.parseActions(actions)
 	end
 end
 
--- CALLBACKS
+-- MODULE
 
-function input.actionActivated(name,isCursorBound,group) end
-function input.actionDeactivated(name,isCursorBound,group) end
-
--- MISC
-
-function input.activate(action)
-	action.active = true
-	input.actionActivated(action.name, action.group, action.isCursorBound)
+local Module
+do
+	Module = Class()
+	function Module:initialize(buttonVerifier)
+		self.aliases = {}
+		self.triggers = {}
+		self.is = buttonVerifier
+	end
+	
+	function Module:verify(button)
+		if self.aliases[button] then
+			button = self.aliases[button]
+		end
+		if self:is(button) then
+			return button
+		else
+			return false
+		end
+	end
 end
 
-function input.deactivate(action)
-	action.active = false
-	input.actionDeactivated(action.name, action.group, action.isCursorBound)
+-- MODULE WORK
+
+function input.addModule(name,buttonVerifier,aliases)
+	input.modules[name] = Module:new(buttonVerifier)
+	if aliases then
+		for _,alias in ipairs(aliases) do
+			input.modules[alias] = input.modules[name]
+		end
+	end
 end
 
+function input.addButtonAlias(module,from,to)
+	input.modules[module].aliases[from] = to
+end
+
+-- KEYBOARD
+
+do
+	input.addModule(
+		"keyboard",
+		function(b)
+			-- this works because love.keyboard.isDown() errors when using an invalid keycode
+			-- as of Löve2d 11.3, there's no better way
+			return pcall(function() love.keyboard.isDown(b) end)
+		end,
+		{"key"}
+	)
+
+	function input.keypressed(key, scancode, isrepeat)
+		input.triggerActivation("keyboard",key)
+	end
+
+	function input.keyreleased(key, scancode)
+		input.triggerDeactivation("keyboard",key)
+	end
+end
+
+-- MOUSE
+
+do
+	input.addModule(
+		"mouse",
+		function(b) return pcall(function() love.mouse.isDown(b) end) end,
+		nil
+	)
+
+	function input.mousepressed(x, y, button, istouch, presses)
+		input.triggerActivation("mouse",button)
+	end
+
+	function input.mousereleased(x, y, button, istouch, presses)
+		input.triggerDeactivation("mouse",button)
+	end
+	
+	input.addButtonAlias("mouse","left",1)
+	input.addButtonAlias("mouse","right",2)
+	input.addButtonAlias("mouse","middle",3)
+end
+
+-- TRIGGER HANDLING
+
+function input.triggerActivation(moduleName,button)
+	local t = input.modules[moduleName].triggers[button]
+	if t then
+		for _,action in ipairs(t) do
+			action.active = true
+			input.actionActivated(action.name, action.group, action.isCursorBound)
+		end
+	end
+end
+
+function input.triggerDeactivation(moduleName,button)
+	local t = input.modules[moduleName].triggers[button]
+	if t then
+		for _,action in ipairs(t) do
+			action.active = false
+			input.actionDeactivated(action.name, action.group, action.isCursorBound)
+		end
+	end
+end
 
 return input
