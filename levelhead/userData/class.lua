d2911d1934504b8f04ddf99fd1673c14be38c064
tyoeer <tyoeer@users.noreply.github.com>
2020-06-26 20:23:19 +0200

    Made UserData.getWorkshopLevels() include the path to the level file

--- levelhead/userData/class.lua
+++ levelhead/userData/class.lua
@@ -12,12 +12,13 @@ end
 
 function D:getWorkshopLevels()
 	local out = {}
-	for _,v in ipairs(self.raw.workshop.my_creations) do
-		local data = self.raw.workshop.my_workshop[v]
+	for _,id in ipairs(self.raw.workshop.my_creations) do
+		local data = self.raw.workshop.my_workshop[id]
 		table.insert(out,{
 			name = require("levelhead.misc").parseLevelName(data.nameparts),
-			id = v,
+			id = id,
 			iconName = data.icon,
+			path = require("levelhead.misc").getUserDataPath().. self.lookupCode .."/Stages/".. id ..".lhs",
 		})
 	end
 	return out
