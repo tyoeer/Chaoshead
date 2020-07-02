065bd20a08a55018cd47689ea74265fcb70eb1a2
tyoeer <tyoeer@users.noreply.github.com>
2020-07-02 11:40:18 +0200

    Added extra checks to mappings in order to return better $error values, and added support for the Music mapping

--- levelhead/data/properties.lua
+++ levelhead/data/properties.lua
@@ -1,4 +1,6 @@
 local csv = require("utils.csv")
+local M = require("levelhead.data.music"):new()
+
 local P = Class(require("levelhead.data.base"))
 
 local SIMPLE_MAPPING_SIZE = 8
@@ -22,6 +24,10 @@ function P:initialize()
 			self.headers.id = v
 		elseif raw:match("mappingtype") then
 			self.headers.mappingType = v
+		elseif raw:match("min") then
+			self.headers.min = v
+		elseif raw:match("max") then
+			self.headers.max = v
 		else
 			for i=-1,SIMPLE_MAPPING_SIZE,1 do
 				if raw:match(string.depatternize("map"..tostring(i))) then
@@ -40,25 +46,53 @@ function P:getMappingType(selector)
 	return self:getRow(selector)[self.headers.mappingType] or "$UnknownMappingType"
 end
 
+function P:getMin(selector)
+	return self:getRow(selector)[self.headers.min] or "$UnknownMinimum"
+end
+
+function P:getMax(selector)
+	return self:getRow(selector)[self.headers.min] or "$UnknownMaximum"
+end
+
 function P:mappingToValue(selector, mapping)
-	if type(mapping)=="number" then
-		return mapping
-	end
 	local p = self:getRow(selector)
-	if not p[self.headers.mappingType] then
+	
+	local mappingType = p[self.headers.mappingType]
+	if mappingType=="None" then
+		return tonumber(mappnig)
+	elseif mappingType=="Simple" or mappingType=="Hybrid" then
+		for i=-1,SIMPLE_MAPPING_SIZE,1 do
+			if mapping == p[self.headers.map[i]] then
+				return i
+			end
+		end
+	elseif mappingType=="Music" then
+		return M:getID(mapping)
+	else
 		print(selector.." has unknown mapping!")
 		return "$UnknownMapping"
 	end
-	for i=-1,SIMPLE_MAPPING_SIZE,1 do
-		if mapping == p[self.headers.map[i]] then
-			return i
-		end
-	end
+	
 	error("Illegal mapping value for "..selector..": "..tostring(mapping).." ("..type(mapping)..")")
 end
 
 function P:valueToMapping(selector, value)
 	local p = self:getRow(selector)
+	
+	if type(value)~="number" then
+		print("Illegal value: "..selector.." :-: "..tostring(value))
+		return "$IllegalValue: "..tostring(value)
+	end
+	
+	local min, max = p[self.headers.min], p[self.headers.max]
+	if not min or not max then
+		print("Property range undefined: "..selector.." :-: "..tostring(min).." - "..tostring("max"))
+		return "$RangeUndefined"
+	end
+	if value < min or value > max then
+		return "$OutOfBOunds"
+	end
+	
 	local mappingType = p[self.headers.mappingType]
 	if mappingType=="None" then
 		return value
@@ -74,6 +108,8 @@ function P:valueToMapping(selector, value)
 		else
 			return m
 		end
+	elseif mappingType=="Music" then
+		return M:getName(value)
 	else
 		print("Illegal mapping type: "..selector.." :-: "..mappingType)
 		return "$UnknownMapping"
