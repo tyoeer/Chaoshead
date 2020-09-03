b2a4d92d268b482beb7259b70f9b0b671cdeaa1d
tyoeer <tyoeer@users.noreply.github.com>
2020-09-03 16:22:23 +0200

    Added campaign (de)compression buttons to the misc tab

--- ui/misc.lua
+++ ui/misc.lua
@@ -8,6 +8,33 @@ function UI:initialize()
 		love.system.openURL(url)
 	end)
 	
+	list:addButtonEntry("Decompress campaign (move the hardfile to the chaoshead data folder first)",function()
+		local i =love.filesystem.getInfo("campaign_hardfile")
+		if i then
+			local c = love.filesystem.read("campaign_hardfile")
+			print("Beginning decompression...")
+			local d = love.data.decompress("data","zlib",c)
+			print("Decompressed! Writing to disk...")
+			love.filesystem.write("campaign.bin",d)
+			print("Done!")
+		else
+			print("No campaign_hardfile found!")
+		end
+	end)
+	list:addButtonEntry("Compress campaign (from campaign.bin in the chaoshead data folder)",function()
+		local i =love.filesystem.getInfo("campaign.bin")
+		if i then
+			local c = love.filesystem.read("campaign.bin")
+			print("Beginning compression...")
+			local d = love.data.compress("data","zlib",c,9)
+			print("Compressed! Writing to disk...")
+			love.filesystem.write("campaign_hardfile",d)
+			print("Done!")
+		else
+			print("No campaign.bin found!")
+		end
+	end)
+	
 	UI.super.initialize(self,list,settings.dim.misc.padding)
 	self.title = "Misc."
 end
