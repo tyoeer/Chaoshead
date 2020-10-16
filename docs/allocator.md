48bd90a751b4bc112450f9c8b1d779c9ae7c8884
tyoeer <tyoeer@users.noreply.github.com>
2020-10-16 18:53:44 +0200

    Added the ability to allocate areas (and masks)

--- docs/allocator.md
+++ docs/allocator.md
@@ -5,14 +5,23 @@ Objects are allocating in the order of (relay) execution.
 Currently expects an empty level, and has no support for appending an existing level (yet).
 
 ## Creation
+
 ```Lua
 instance = require("tools.allocator"):new(level,settings)
 ```
 - level: the level to allocate stuff in
 - settings: a table containing the settings:
-	- immediate: whether the allocator should place objects immediatly upon allocation, or wait untill allocator:finalize(). Default is `false`.
+	- immediate: whether the allocator should place objects immediatly upon allocation, or wait untill allocator:finalize().
+	  Default is `false`.
+	- size: table containing the size of the area in which things should be allocated in the format `{width,height}`.
+	  Use together with `setTopLeftCorner()` to limit the allocator to a specific area.
+	  Defaults to the level size.
+	- mask: wether the allocator should have a mask that limits where things can be placed.
+	  See `getMask()` for more info.
+	  Defaults to `false`
 
 ## Allocating
+
 ```Lua
 allocator:allocateRelay(receivingChannel, switchRequirements, sendingChannel)
 ```
@@ -24,13 +33,21 @@ obj = allocator:allocateObject(element)
 - element: the name or id of the level element to allocate
 - obj: an object that the allocator will make sure gets placed somewhere
 
-
 ```Lua
 channel = allocater:allocateChannel()
 ```
 - channel: a new unused channel
 
+```Lua
+area = allocator:allocateArea(width, height)
+```
+- width, height: the size fo the area to allocate
+- area: an allocator limited to the spcified area
+
+There're no guarantees about the order in which areas are allocated, relating to objects and each other.
+
 ## Misc
+
 ```Lua
 ralloc, calloc = allocator:getShortcuts()
 ```
@@ -41,4 +58,18 @@ ralloc, calloc = allocator:getShortcuts()
 allocator:finalize()
 ```
 Actually places all objects when the immediate setting is off/false, errors otherwise.
-Should only be called once.
+
+```Lua
+mask = allocator:getMask()
+```
+- mask: (a reference to) the Bitplane masking where the allocator can place objects
+
+Set a position to `false` to block it off from the allocator. All spaces default to `true`.
+
+```Lua
+allocator:setTopLeftCorner(x,y)
+```
+- x, y: the position of the top-left corner (inclusive)
+
+Sets the top-left corner of the area in which things should be allocated.
+Use togetether with the `size` setting to limit the allocator to a specific area.
--- tools/allocator.lua
+++ tools/allocator.lua
@@ -3,21 +3,46 @@ local P = require("levelhead.objects.propertiesBase")
 local A = Class()
 
 function A:initialize(level,settings)
-	if settings.immediate==nil then settings.immediate=false end
 	self.settings = settings
-	if not self.settings.immediate then
-		self.queue = {}
-	end
 	self.level = level
 	
-	self.objX=1
-	self.objY=1
+	if self.settings.immediate==nil then
+		self.settings.immediate=false
+	end
+	if self.settings.mask==nil then
+		self.settings.mask = false
+	end
 	
+	if self.settings.size then
+		self.width = self.settings.size[1]
+		self.height = self.settings.size[2]
+	else
+		self.width = self.level.width
+		self.height = self.level.height
+	end
+	
+	if not self.settings.immediate then
+		self.objectQueue = {}
+		self.areaQueue = {}
+	end
+	if self.settings.mask then
+		self.mask = require("tools.bitplane").new(self.width, self.height, true)
+	end
+	
+	self:setTopLeftCorner(1,1)
+	--[[
+	self.minX
+	self.minY
+	self.freeX
+	self.freeY
+	self.maxX
+	self.maxY
+	]]--
 	self.channelCounter = -1 -- it gets incremented before returning a channel
 end
 
 
--- OBJECTS
+-- OBJECTS & AREAS
 
 
 function A:allocateObject(element)
@@ -25,21 +50,22 @@ function A:allocateObject(element)
 	if self.settings.immediate then
 		self:placeObject(obj)
 	else
-		table.insert(self.queue,obj)
+		table.insert(self.objectQueue,obj)
 	end
 	return obj
 end
 
-function A:placeObject(obj)
-	self.level:addObject(obj, self.objX, self.objY)
-	self.objX = self.objX+1
-	if self.objX > self.level.width then
-		self.objX = 1
-		self.objY = self.objY + 1
-		if self.objY > self.level.height then
-			error("Can't add object: level too small!")
-		end
+function A:allocateArea(w,h)
+	local sub = A:new(self.level,{
+		size={w,h},
+		immediate = self.settings.immediate,
+	})
+	if self.settings.immediate then
+		self:placeArea(sub)
+	else
+		table.insert(self.areaQueue,sub)
 	end
+	return sub
 end
 
 function A:allocateRelay(cin,cond,cout)
@@ -49,14 +75,71 @@ function A:allocateRelay(cin,cond,cout)
 	r:setSendingChannel(cout)
 end
 
+function A:nextPos(x,y)
+	if y > self.maxY then
+		error("Allocation Error: level/area too small!",3)
+	end
+	x = x+1
+	if x > self.maxX then
+		x = self.minX
+		y = y + 1
+	end
+	return x,y
+end
+
+function A:nextFree(x,y)
+	x, y = self:nextPos(x, y)
+	if self.mask then
+		while not self.mask:get(x,y) do
+			x, y = self:nextPos(x, y)
+		end
+	end
+	return x, y
+end
+
+function A:placeObject(obj)
+	self.level:addObject(obj, self.freeX, self.freeY)
+	if self.mask then
+		self.mask:set(self.freeX, self.freeY, false)
+	end
+	self.freeX, self.freeY = self:nextFree(self.freeX, self.freeY)
+end
+
+function A:placeArea(area)
+	local first = true
+	local x = self.freeX
+	local y = self.freeY
+	while true do
+		--check if the area can be placed
+		if not self.mask:rectContains(x,y, area.width, area.height, false) then
+			area:setTopLeftCorner(x,y)
+			self.mask:setRect(x,y, area.width, area.height, false)
+			if not area.settings.immediate then
+				area:finalize()
+			end
+			if first then
+				self.freeX, self.freeY = self:nextFree(self.freeX, self.freeY)
+			end
+			break
+		else
+			x, y = self:nextFree(x, y)
+			first = false
+		end
+	end
+end
+
 function A:finalize()
 	if self.settings.immediate then
 		error("Trying to finalize an immediate allocator!",2)
 	end
-	for _,obj in ipairs(self.queue) do
+	for _,area in ipairs(self.areaQueue) do
+		self:placeArea(area)
+	end
+	for _,obj in ipairs(self.objectQueue) do
 		self:placeObject(obj)
 	end
-	self.queue = {}
+	self.objectQueue = {}
+	self.areaQueue = {}
 end
 
 
@@ -72,6 +155,23 @@ end
 -- MISC
 
 
+function A:getMask()
+	if self.mask then
+		return self.mask
+	else
+		error("Trying to get the mask of a mask-less allocator!",2)
+	end
+end
+
+function A:setTopLeftCorner(x,y)
+	self.minX = x
+	self.minY = y
+	self.freeX = x
+	self.freeY = y
+	self.maxX = x + self.width - 1
+	self.maxY = y + self.height - 1
+end
+
 function A:getShortcuts()
 	return function(...)
 		self:allocateRelay(...)
@@ -79,4 +179,6 @@ function A:getShortcuts()
 		return self:allocateChannel()
 	end
 end
+
+
 return A
