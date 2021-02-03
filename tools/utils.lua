0c6a42415b52ac476aca94a31b1697a0204d4b46
tyoeer <tyoeer@users.noreply.github.com>
2021-02-03 19:02:21 +0100

    Fixed various level utils offsetEverything() errors

--- tools/utils.lua
+++ tools/utils.lua
@@ -13,22 +13,31 @@ end
 local function offsetPathNode(pn,dx,dy,done)
 	done[pn] = true
 	local x,y = pn.x+dx, pn.y+dy
-	local taken = pn.world.pathNodes[x][y]
+	local taken = pn.path.world.pathNodes[x][y]
 	if taken then
-		offsetOPathNode(taken,dx,dy,done)
+		offsetPathNode(taken,dx,dy,done)
 	end
-	pn.world:movePathNode(pn, pn.x+dx, pn.y+dy)
+	pn.path.world:movePathNode(pn, pn.x+dx, pn.y+dy)
 end
 
 function u.offsetEverything(world,dx,dy)
+	if dx==0 and dy==0 then
+		--prevent stack overflow due to object trying to move itself first,
+		--before moving, getting stuck in an infinite loop
+		return
+	end
 	local done = {}
 	for obj in world.objects:iterate() do
-		offsetObject(obj,dx,dy,done)
+		if not done[obj] then
+			offsetObject(obj,dx,dy,done)
+		end
 	end
 	for path in world.paths:iterate() do
 		local node = path.head
 		while node do
-			offsetpathNode(node,dx,dy,done)
+			if not done[node] then
+				offsetPathNode(node,dx,dy,done)
+			end
 			node = node.next
 		end
 	end
