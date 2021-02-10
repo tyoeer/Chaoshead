f901577cd3cb963d29a1d907a7b932ffdb5478a2
tyoeer <tyoeer@users.noreply.github.com>
2021-02-10 19:11:52 +0100

    Fixed crash upon trying to open level

--- ui/chaoshead.lua
+++ ui/chaoshead.lua
@@ -1,13 +1,13 @@
 local UI = Class(require("ui.structure.proxy"))
 
 function UI:initialize(w,h)
-	local tabs = require("ui.structure.tabs"):new()
-	self.modalOverlay = require("ui.structure.overlay"):new(tabs)
+	self.mainTabs = require("ui.structure.tabs"):new()
+	self.modalOverlay = require("ui.structure.overlay"):new(self.mainTabs)
 	
 	UI.super.initialize(self,self.modalOverlay)
 	self:resize(w,h)
 	
-	tabs.tabHeight = settings.dim.main.tabHeight
+	self.mainTabs.tabHeight = settings.dim.main.tabHeight
 	
 	self.nLevels = 0
 	self.levels = require("ui.structure.tabs"):new()
@@ -15,13 +15,13 @@ function UI:initialize(w,h)
 	self.noLevelsUI = require("ui.list.text"):new("No levels opened!")
 	self.noLevelsUI.title = "Level Editors"
 	self.levelsProxy = require("ui.structure.proxy"):new(self.noLevelsUI)
-	tabs:addChild(self.levelsProxy)
+	self.mainTabs:addChild(self.levelsProxy)
 	
 	local levelSelector = require("ui.levelSelector"):new()
-	tabs:addChild(levelSelector)
-	tabs:setActive(levelSelector)
+	self.mainTabs:addChild(levelSelector)
+	self.mainTabs:setActive(levelSelector)
 	
-	tabs:addChild(require("ui.misc"):new())
+	self.mainTabs:addChild(require("ui.misc"):new())
 	
 end
 
@@ -31,7 +31,7 @@ function UI:openEditor(path,name)
 	editor.title = name
 	self.levels:addChild(editor)
 	self.levels:setActive(editor)
-	self.child:setActive(self.levelsProxy)
+	self.mainTabs:setActive(self.levelsProxy)
 	self.nLevels = self.nLevels + 1
 	if self.nLevels == 1 then
 		self.levelsProxy:setChild(self.levels)
