f4b01077660c5b57a19fc633d683217d5497a37c
tyoeer <tyoeer@users.noreply.github.com>
2021-04-27 20:44:31 +0200

    Added UI:getMinimumHeight() test to misc tab

--- ui/misc.lua
+++ ui/misc.lua
@@ -75,6 +75,25 @@ function UI:initialize()
 		print(list)
 		print("----------")
 	end)
+	list:addButtonEntry("UI testing time, or WHY DOES getMinimumHeight() NOT WORK PROPERLY?!?!?!?!",function()
+		local buttons = require("ui.structure.list"):new()
+		buttons:addTextEntry(" ",0)
+		buttons:addTextEntry(" ",0)
+		buttons:addTextEntry(" ",0)
+		for i=1,100,1 do
+			local button
+			if i%2==0 then
+				button = require("ui.list.text"):new(i,i%7)
+			else
+				button = require("ui.list.button"):new(i,i%9)
+			end
+			button = require("ui.structure.padding"):new(button,(i%5)*10)
+			buttons:addUIEntry(button)
+		end
+		buttons = require("ui.structure.scrollbar"):new(buttons)
+		ui.child.child:addChild(buttons)
+	end)
+	
 	
 	UI.super.initialize(self,list,settings.dim.misc.miscPadding)
 	self.title = "Misc."
