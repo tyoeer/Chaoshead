81e0dd36778a7ed0a4e8b097a7bac68da9376be9
Arad Arbel <aradarbel10@gmail.com>
2021-07-13 23:49:31 +0300

    added level index to unnamed levels in selection menu

--- levelhead/userData/class.lua
+++ levelhead/userData/class.lua
@@ -15,7 +15,7 @@ function D:getWorkshopLevels()
 	for _,id in ipairs(self.raw.workshop.my_creations) do
 		local data = self.raw.workshop.my_workshop[id]
 		table.insert(out,{
-			name = require("levelhead.misc").parseLevelName(data.nameparts),
+			name = require("levelhead.misc").parseLevelName(data.nameparts):gsub("$Unnamed Level", "$Unnamed Level".." ["..id.."]"),
 			id = id,
 			iconName = data.icon,
 			path = require("levelhead.misc").getUserDataPath().. self.lookupCode .."/Stages/".. id ..".lhs",
