251d3c40247ee839212a845143ef5a976ec583e5
tyoeer <tyoeer@users.noreply.github.com>
2021-08-17 16:21:28 +0200

    Removed no longer used tab color settings

--- settings/colors.lua
+++ settings/colors.lua
@@ -29,21 +29,6 @@ return {
 		},
 		selectionAreaOutline = {1,1,1, 1},
 	},
-	tabs = {
-		divide = {1,1,1, 1},
-		hover = {
-			bg = {1,1,1, 0.5},
-			text = {1,1,1, 1},
-		},
-		selected = {
-			bg = {1,1,1, 0.3},
-			text = {1,1,1, 1},
-		},
-		other = {
-			bg = {0,0,0, 1},
-			text = {1,1,1, 1},
-		},
-	},
 	horDivide = {1,1,1, 1},
 	list = {
 		text = {1,1,1, 1},
