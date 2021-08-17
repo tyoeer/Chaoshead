1414515ada5cf121019cb91572656bc51adb0fba
tyoeer <tyoeer@users.noreply.github.com>
2021-08-17 16:11:32 +0200

    Made UI container event hooks get called after the children get the event, and removed event cancellation

--- ui/base/container.lua
+++ ui/base/container.lua
@@ -32,11 +32,10 @@ end
 local function rAll(event)
 	local hook = "on"..event:sub(1,1):upper()..event:sub(2,-1)
 	UI[event] = function(self,...)
-		if not self[hook](self,...) then
-			for _,child in ipairs(self.children) do
-				child[event](child,...)
-			end
+		for _,child in ipairs(self.children) do
+			child[event](child,...)
 		end
+		self[hook](self,...)
 	end
 	--stub the hook
 	UI[hook] = function() end
@@ -47,20 +46,20 @@ end
 --rAll("resized")
 
 rAll("update")
+
 function UI:draw()
-	if not self:onDraw() then
-		for _,child in ipairs(self.children) do
-			love.graphics.push("all")
-				love.graphics.translate(child.x, child.y)
-				--find childs bounding box in screen coordinates
-				local startX,startY = love.graphics.transformPoint(0,0)
-				local endX,endY = love.graphics.transformPoint(child.width, child.height)
-				--make sure the child can't draw outside it's bounding box
-				love.graphics.intersectScissor(startX, startY, endX-startX, endY-startY)
-				child:draw()
-			love.graphics.pop("all")
-		end
+	for _,child in ipairs(self.children) do
+		love.graphics.push("all")
+			love.graphics.translate(child.x, child.y)
+			--find childs bounding box in screen coordinates
+			local startX,startY = love.graphics.transformPoint(0,0)
+			local endX,endY = love.graphics.transformPoint(child.width, child.height)
+			--make sure the child can't draw outside it's bounding box
+			love.graphics.intersectScissor(startX, startY, endX-startX, endY-startY)
+			child:draw()
+		love.graphics.pop("all")
 	end
+	self:onDraw()
 end
 function UI:onDraw() end
 
@@ -68,59 +67,57 @@ rAll("focus")
 rAll("visible")
 
 function UI:inputActivated(name,group, isCursorBound)
-	if not self:onInputActivated(name,group, isCursorBound) then
-		if isCursorBound then
-			local child = self:findChildAt(self:getMousePos())
-			if child then
-				child:inputActivated(name,group, isCursorBound)
-			end
-		else
-			for _,child in ipairs(self.children) do
-				child:inputActivated(name,group, isCursorBound)
-			end
+	if isCursorBound then
+		local child = self:findChildAt(self:getMousePos())
+		if child then
+			child:inputActivated(name,group, isCursorBound)
+		end
+	else
+		for _,child in ipairs(self.children) do
+			child:inputActivated(name,group, isCursorBound)
 		end
 	end
+	self:onInputActivated(name,group, isCursorBound)
 end
 function UI:onInputActivated() end
+
 function UI:inputDeactivated(name,group, isCursorBound)
-	if not self:onInputDeactivated(name,group, isCursorBound) then
-		if isCursorBound then
-			local child = self:findChildAt(self:getMousePos())
-			if child then
-				child:inputDeactivated(name,group, isCursorBound)
-			end
-		else
-			for _,child in ipairs(self.children) do
-				child:inputDeactivated(name,group, isCursorBound)
-			end
+	if isCursorBound then
+		local child = self:findChildAt(self:getMousePos())
+		if child then
+			child:inputDeactivated(name,group, isCursorBound)
+		end
+	else
+		for _,child in ipairs(self.children) do
+			child:inputDeactivated(name,group, isCursorBound)
 		end
 	end
+	self:onInputDeactivated(name,group, isCursorBound)
 end
 function UI:onInputDeactivated() end
 
 rAll("textInput")
 
 function UI:mouseMoved(x,y, dx,dy)
-	if not self:onMouseMoved(x,y, dx,dy) then
-		--also give the event to the child the mouse moved out from
-		local oldChild = self:findChildAt(x-dx, y-dy)
-		if oldChild then
-			oldChild:mouseMoved(x-oldChild.x, y-oldChild.y, dx,dy)
-		end
-		local newChild = self:findChildAt(x,y)
-		if newChild and newChild ~= oldChild then
-			newChild:mouseMoved(x-newChild.x, y-newChild.y, dx,dy)
-		end
+	--also give the event to the child the mouse moved out from
+	local oldChild = self:findChildAt(x-dx, y-dy)
+	if oldChild then
+		oldChild:mouseMoved(x-oldChild.x, y-oldChild.y, dx,dy)
 	end
+	local newChild = self:findChildAt(x,y)
+	if newChild and newChild ~= oldChild then
+		newChild:mouseMoved(x-newChild.x, y-newChild.y, dx,dy)
+	end
+	self:onMouseMoved(x,y, dx,dy)
 end
 function UI:onMouseMoved() end
+
 function UI:wheelMoved(dx,dy)
-	if not self:onWheelMoved(dx,dy) then
-		local child = self:findChildAt(self:getMousePos())
-		if child then
-			child:wheelMoved(dx,dy)
-		end
+	local child = self:findChildAt(self:getMousePos())
+	if child then
+		child:wheelMoved(dx,dy)
 	end
+	self:onWheelMoved(dx,dy)
 end
 function UI:onWheelMoved() end
 
