89f71153ca7e2dd4a385de745f94b0b857ee4003
tyoeer <tyoeer@users.noreply.github.com>
2021-08-22 20:36:05 +0200

    Added support for a nil child to the UI proxy

--- ui/base/proxy.lua
+++ ui/base/proxy.lua
@@ -3,17 +3,24 @@ local UI = Class("ProxyUI",require("ui.base.container"))
 function UI:initialize(child)
 	UI.super.initialize(self)
 	--self.child
-	self:setChild(child)
+	if child then
+		self:setChild(child)
+	end
 end
 
 function UI:setChild(child)
 	if self.child then
-		self:removeChild(self.child)
+		self:unsetChild()
 	end
 	self.child = child
 	self:addChild(child)
 	child:resize(self.width, self.height)
 	child:move(0,0)
+end
+
+function UI:unsetChild()
+	self:removeChild(self.child)
+	self.child = nil
 end
 
 function UI:resized(w,h)
