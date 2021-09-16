594ed13dad31136e5c7c2217965fba03e3e4f77f
tyoeer <tyoeer@users.noreply.github.com>
2021-09-16 15:25:59 +0200

    Made boxes use a style argument

--- ui/layout/box.lua
+++ ui/layout/box.lua
@@ -1,17 +1,27 @@
 local UI = Class("BoxUI",require("ui.layout.padding"))
 
-function UI:initialize(child)
-	UI.super.initialize(self, child, settings.dim.misc.boxPadding)
+function UI:initialize(child,style)
+	if not style.padding then
+		error("Padding not specified!",2)
+	end
+	if not style.backgroundColor then
+		error("Background color not specified!",2)
+	end
+	if not style.borderColor then
+		error("Border color not specified!",2)
+	end
+	self.style = style
+	UI.super.initialize(self, child, style.padding)
 end
 
 function UI:preDraw()
-	love.graphics.setColor(settings.col.box.bg)
+	love.graphics.setColor(self.style.backgroundColor)
 	love.graphics.rectangle(
 		"fill",
 		0, 0,
 		self.width, self.height
 	)
-	love.graphics.setColor(settings.col.box.border)
+	love.graphics.setColor(self.style.borderColor)
 	love.graphics.rectangle(
 		"line",
 		0.5, 0.5,
