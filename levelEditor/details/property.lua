68c161b0a7211c4f5ebe1465bc1a1d47bf8fc9a5
tyoeer <tyoeer@users.noreply.github.com>
2022-02-16 17:29:22 +0100

    Put property details on 1 line if there's space

--- levelEditor/details/property.lua
+++ levelEditor/details/property.lua
@@ -1,7 +1,8 @@
 local P = require("levelhead.data.properties")
-local E = require("levelhead.data.elements")
 
 local UI = Class(require("ui.layout.list"))
+
+local SEPERATOR =":  "
 
 function UI:initialize(propertyList,listStyle)
 	UI.super.initialize(self,listStyle)
@@ -14,17 +15,54 @@ function UI:setPropertyList(propertyList)
 	self:reload()
 end
 
+function UI:getName()
+	local pl = self.propertyList
+	return string.format("%s (%d)", P:getName(pl.propId), pl.propId)
+end
+
+function UI:getValues()
+	local pl = self.propertyList
+	if pl.min==pl.max then
+		return string.format("%s (%d)",P:valueToMapping(pl.propId,pl.min),pl.min)
+	else
+		return string.format("%s-%s (%d-%d)",P:valueToMapping(pl.propId,pl.min),P:valueToMapping(pl.propId,pl.max),pl.min,pl.max)
+	end
+end
+
 function UI:reload()
+	self:reloadWithWidth(self.width)
+end
+
+function UI:reloadWithWidth(width)
 	self:resetList()
 	if self.propertyList then
-		local pl = self.propertyList
-		self:addTextEntry(string.format("%s (%d)",P:getName(pl.propId),pl.propId),0)
-		if pl.min==pl.max then
-			self:addTextEntry(string.format("%s (%d)",P:valueToMapping(pl.propId,pl.min),pl.min),1)
+		local name = self:getName()
+		local values = self:getValues()
+		local both = name..SEPERATOR..values
+		local _, lines = love.graphics.getFont():getWrap(both, width)
+		if #lines > 1 then
+			self:addTextEntry(name)
+			self:addTextEntry(values, 1)
 		else
-			self:addTextEntry(string.format("%s-%s (%d-%d)",P:valueToMapping(pl.propId,pl.min),P:valueToMapping(pl.propId,pl.max),pl.min,pl.max),1)
+			self:addTextEntry(both)
 		end
 	end
+end
+
+function UI:getMinimumHeight(width)
+	--Workaround until I make a better system for getMinimumHeight in list when their items might change depending on their width
+	-- Probable solution: don't use a list
+	self:reloadWithWidth(width)
+	local out = UI.super.getMinimumHeight(self, width)
+	self:reloadWithWidth(self.width)
+	return out
+end
+
+function UI:resized(...)
+	UI.super.resized(self, ...)
+	--reload on resize so we can change whether or not some properties and their values
+	-- are put on 1 line or 2
+	self:reload()
 end
 
 return UI
