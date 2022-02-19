9b1c3d17e98edd4e35a00385df25f09e59c5eb58
tyoeer <tyoeer@users.noreply.github.com>
2022-02-19 12:06:36 +0100

    Fixed error message not getting formatted properly

--- levelhead/dataFile.lua
+++ levelhead/dataFile.lua
@@ -6,7 +6,7 @@ local DataFile = Class("DataFile")
 function DataFile:initialize(fullPath)
 	local raw, err = NFS.read(fullPath)
 	if not raw then
-		error(string.format("Error reading file at &q: %s",fullPath,err))
+		error(string.format("Error reading file at %q: %s",fullPath,err))
 	end
 	local jsonData, mystery, hash = raw:match("([^\r\n]+)[\r\n]([^\r\n]+)[\r\n]([^\r\n]+)")
 	self.raw = require("libs.json").decode(jsonData)
