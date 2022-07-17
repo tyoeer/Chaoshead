7a37643602fc34a31c32cb04b4299d7e67d5e132
tyoeer <tyoeer@users.noreply.github.com>
2022-07-17 21:34:04 +0200

    Added a popup at start  in case the LH data folder couldn't be found

--- ui/chaoshead.lua
+++ ui/chaoshead.lua
@@ -1,8 +1,9 @@
---local LEVEL_ROOT = require("ui.level.levelRoot")
 local TABS = require("ui.tools.tabs")
 local WORKSHOP = require("levelEditor.workshop.workshop")
 local DATA_EXPLORER = require("dataExplorer.overview")
 local MISC = require("ui.misc")
+local LH_MISC = require("levelhead.misc")
+local NFS = require("libs.nativefs")
 
 local UI = Class(require("ui.tools.modal"))
 
@@ -21,11 +22,19 @@ function UI:initialize()
 	
 	--TabsUI needs buttons before getting resized (which always happens when added)
 	UI.super.initialize(self,self.mainTabs)
+	
+	self:verifyDataFound()
 end
 
 function UI:draw()
 	love.graphics.clear(Settings.theme.main.background)
 	UI.super.draw(self)
+end
+
+function UI:verifyDataFound()
+	if not NFS.getInfo(LH_MISC.getDataPath(),"directory") then
+		self:displayMessage("Could not find Levelhead data at "..LH_MISC.getDataPath())
+	end
 end
 
 return UI
