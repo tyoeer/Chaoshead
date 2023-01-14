718d341998e371a18680a220831f5cf3c7b165aa
tyoeer <tyoeer@users.noreply.github.com>
2023-01-14 17:00:44 +0100

    Campaign: add type annotations to Mapped

--- levelhead/campaign/mapped.lua
+++ levelhead/campaign/mapped.lua
@@ -1,18 +1,35 @@
+---A class that provides mappings to convert itself from/to a "mapped" version. The non-mapped version is referred to as "object".
+--- You can just provide the field name as a string instead of this class if you don't have any mapping to do
+---@class Mapping
+---@field [1] string The field in the mapped object the object field gets mapped to
+---@field to nil|fun(objectField: unknown): unknown A function to map the object value to the mapped value
+---@field from nil|fun(mappedField: unknown): unknown A function to map the mapped value to the object value
+---@field optional boolean? If a non-optional field is missing, an error is thrown. Optional fields will still get mapping functions called. Defaults to false.
+
+---@alias Mappings table<string, Mapping> the string index is 
+
+
+---@class Mapped : Object
+---@field new fun(self, ...: Mappings): self
 local JM = Class("Mapped")
 
+---@param ... Mappings
 function JM:initialize(...)
+	---@type Mappings
 	self.mappings = {}
 	for _,mapping in ipairs({...}) do
 		self:extendMappings(mapping)
 	end
 end
 
+---@param extra Mappings
 function JM:extendMappings(extra)
 	for field, mapping in pairs(extra) do
 		self.mappings[field] = mapping
 	end
 end
 
+--- Fills this object in with mapped data from src
 function JM:fromMapped(src)
 	for field, mapping in pairs(self.mappings) do
 		if type(mapping)=="string" then
@@ -35,6 +52,7 @@ function JM:fromMapped(src)
 	end
 end
 
+--- Creates a new table with mapped data from this object
 function JM:toMapped()
 	local dst = {}
 	for field, mapping in pairs(self.mappings) do
@@ -65,6 +83,10 @@ end
 local boolFrom = function(raw)
 	return raw and raw>0.5 or false
 end
+
+--- Generate the Mapping to map a number to a bool in the object.
+---@param field string
+---@param optional boolean
 function JM.mapBool(field, optional)
 	if type(field)=="table" then
 		error("Don't use OOP call!", 2)
