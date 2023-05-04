747b3fd313d708af8b0a671140ad4d2f01093ae6
tyoeer <tyoeer@users.noreply.github.com>
2023-05-04 13:02:46 +0200

    Fixed CH crashing if a user has a folder for save_data

--- levelhead/userData/init.lua
+++ levelhead/userData/init.lua
@@ -17,8 +17,11 @@ end
 
 function UD.getUserData(code)
 	local path = require("levelhead.misc").getUserDataPath()..code..dataFilePath
-	if NFS.getInfo(path) then
+	local info = NFS.getInfo(path)
+	if info and info.type=="file" then
 		return UD.class:new(path,code)
+	elseif info then
+		return nil, string.format("Invalid userdata found for %q: save_data is a %s", code, info.type)
 	else
 		return nil, string.format("No userdata found for %q", code)
 	end
