f9afe013264e86e8dc7377e1cab45a63dfcc793d
tyoeer <tyoeer@users.noreply.github.com>
2023-08-26 18:19:40 +0200

    Internal: added class annotation to Level

--- levelhead/level/level.lua
+++ levelhead/level/level.lua
@@ -1,6 +1,8 @@
 local World = require("levelhead.level.world")
 local Settings = require("levelhead.level.settings")
 
+---@class Level : World
+---@field super World
 local Level = Class(World)
 
 function Level:initialize(w,h)
