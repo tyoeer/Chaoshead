81e9daed8d360a95c3af7c01c73104e8e4878070
tyoeer <tyoeer@users.noreply.github.com>
2023-08-28 22:29:39 +0200

    Type: rename Settings -> Level.Settings to reduce confusion with CH settings

--- levelhead/level/settings.lua
+++ levelhead/level/settings.lua
@@ -3,7 +3,7 @@ local Zones = require("levelhead.data.zones")
 local Lang = require("levelhead.data.languages")
 local Misc = require("levelhead.misc")
 
----@class Settings : Class
+---@class Level.Settings : Class
 local Settings = Class("LevelSettings")
 
 function Settings:initialize()
