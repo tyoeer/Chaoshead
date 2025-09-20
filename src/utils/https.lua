ab8a91888220ea2f54a246fa44fc7748148e8b25
tyoeer <tyoeer@users.noreply.github.com>
2025-09-21 01:17:22 +0200

    Update error message for missing HTTPS lib

--- src/utils/https.lua
+++ src/utils/https.lua
@@ -10,8 +10,14 @@ else
 		request = function(url, options)
 			return 418, "ERROR: Chaoshead failed to load its own HTTPS library, and is now pretending to be a teapot "
 			.."instead because I couldn't find a fitting status code.\n\n"
-			.."If you're a developer, grab a https dynamic library from https://github.com/love2d/lua-https/actions/workflows/build.yml "
-			.."and put in the Chaoshead root directory (if you're not a developer, report this as a bug).\n\n"
+			-- TODO update to consider that we have most https libraries
+			.."If you're a user, report this as a bug.\n\n"
+			.."If you're a developer:\n"
+			.." - If you're on Windows x32: rename https32.dll to https.dll (you'll have to dispose the old https.dll)\n"
+			.." - If you're on Windows x64 or Linux: wut. Something broke, the HTTPS libraries are included in the Git repository.\n"
+			.." - If you're on another system (MacOS): "
+			.."grab a https dynamic library for your system from https://github.com/love2d/lua-https/actions/workflows/build.yml "
+			.."and put in the Chaoshead root directory.\n\n"
 			..tostring(httpsOrError), ""
 		end,
 		error = httpsOrError,
